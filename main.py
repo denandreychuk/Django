825a432ddc43f55276445dd0e5408c66465b1760
Денис Андрейчук <denis.andrei4uk@yandex.ua>
2020-12-06 16:33:24 +0200

    Development: Django initial setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import django
+
+def print_hi(name):
+    print(django.get_version())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
